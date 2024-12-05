8d82086d957955678747e76dade8fad0ff50b8c1
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2024-12-05 22:32:21 +0300

    RegisterPostEvictionCallback

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -5,7 +5,7 @@ namespace InMemoryApp.Web.Controllers
 {
     public class ProductController : Controller
     {
-        private  IMemoryCache _memoryCache;
+        private IMemoryCache _memoryCache;
         public ProductController(IMemoryCache memoryCache)
         {
             _memoryCache = memoryCache;
@@ -15,12 +15,18 @@ namespace InMemoryApp.Web.Controllers
             if (!_memoryCache.TryGetValue("zaman", out string? zamancache))
             {
                 MemoryCacheEntryOptions options = new();
-                
-               options.SlidingExpiration = TimeSpan.FromSeconds(10);
-               options.AbsoluteExpiration= DateTime.Now.AddMinutes(1);
-               options.Priority=CacheItemPriority.High
 
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString(),options);
+                options.SlidingExpiration = TimeSpan.FromSeconds(10);
+                options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+                options.Priority = CacheItemPriority.High;
+
+                options.RegisterPostEvictionCallback((key, value, reason, state) =>
+                {
+                    _memoryCache.Set("callback", $"{key}->{value} => sebep:{reason}");
+
+                });
+
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
             }
 
             return View();
@@ -29,7 +35,9 @@ namespace InMemoryApp.Web.Controllers
         public IActionResult Get()
         {
             _memoryCache.TryGetValue("zaman", out string? zamancache);
+            _memoryCache.TryGetValue("callback", out string? callback);
 
+            ViewBag.callback = callback;
             ViewBag.zaman = zamancache;
 
             return View();
