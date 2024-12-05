5cd290dca945cd96e304f096c30288e651d4e18f
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2024-12-05 21:56:10 +0300

    TryGetValue() & Remove()

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -12,13 +12,33 @@ namespace InMemoryApp.Web.Controllers
         }
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("zaman",DateTime.Now.ToString());
+            //memorydeki datanın var olup olmadığını kontrol ediyoruz 1. yol
+            if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            //2. yol TryGetValue ile birlikte belirtilen keyi alıp objeye atamasını yapıyoruz
+            if (!_memoryCache.TryGetValue("zaman", out string zamancache))
+            {
+                //eğer memoryde yoksa memorye ekliyoruz
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
 
             return View();
         }
 
         public IActionResult Get()
         {
+            //Remove ile memorydeki verilen key'e göre data silinir
+            _memoryCache.Remove("zaman");
+
+            //GetOrCreate ile memoryde var sa alıyoruz yoksa oluşturuyoruz.
+            _memoryCache.GetOrCreate<string>("zaman", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+
             ViewBag.zaman = _memoryCache.Get<string>("zaman");
 
             return View();
