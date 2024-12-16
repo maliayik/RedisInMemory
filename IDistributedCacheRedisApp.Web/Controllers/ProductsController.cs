2af2e7ba904d6afde2d0b10ce29134d393f64b86
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2024-12-16 20:52:10 +0300

    Basit dataların kaydedilmesi.

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -5,10 +5,33 @@ namespace IDistributedCacheRedisApp.Web.Controllers
 {
     public class ProductsController(IDistributedCache _distributedCache) : Controller
     {
+        // data kaydetme işlemi
         public IActionResult Index()
         {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+
+            //1 dakikalık bir cache süresi belirledik
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            _distributedCache.SetString("name","Mehmet Ali", cacheEntryOptions);
             
             return View();
         }
+
+        //okuma işlemi
+        public IActionResult Show()
+        {
+            string name = _distributedCache.GetString("name");
+            ViewBag.name = name;
+            return View();
+        }
+
+        //silme işlemi
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+            return View();
+        }
+
     }
 }
