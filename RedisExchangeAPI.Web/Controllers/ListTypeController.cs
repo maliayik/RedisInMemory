391d394e8fcf3839faa71250b1497893810e40a7
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2024-12-23 23:18:07 +0300

    ListType Initialize

---
+++ RedisExchangeAPI.Web/Controllers/ListTypeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        private string listKey = "names";
+        public ListTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(1);
+        }
+        
+        //kaydedilen datayı okumak için.
+        public IActionResult Index()
+        {
+            List<string> namesList = new List<string>();
+
+            if (db.KeyExists(listKey))
+            {
+                db.ListRange(listKey).ToList().ForEach(x =>
+                {
+                    namesList.Add(x.ToString());
+                });
+            }
+
+            return View(namesList);
+        }
+
+        //data kaydetme (ekleme metodu)
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            //listenin sonuna eleman ekler.
+            db.ListRightPush(listKey, name);
+           
+            return RedirectToAction("Index");
+        }
+        
+        //silme işlemi
+        public  IActionResult DeleteItem(string name)
+        {
+             db.ListRemoveAsync(listKey, name).Wait();
+
+            return RedirectToAction("Index");
+        }
+
+        //listenin solundaki ilk elamnı silen metot
+        public IActionResult DeleteFirstItem()
+        {
+            db.ListLeftPop(listKey);
+            return RedirectToAction("Index");
+        }
+    }
+}
